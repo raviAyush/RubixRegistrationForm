8c244e5dadd11f1a549c1427a1a277953b6d2e91
raviAyush <48278868+raviAyush@users.noreply.github.com>
2022-12-28 15:07:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,111 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const initialValues = { username: "", email: "", phoneNo: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.username) {
+      errors.username = "Username is required!";
+    }
+    if (!values.email) {
+      errors.email = "Email is required!";
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email format!";
+    }
+    if (!values.phoneNo) {
+      errors.phoneNo = "phoneNo is required";
+    } else if (values.phoneNo.length < 10) {
+      errors.phoneNo = "phoneNo must be 10 digite";
+    } else if (values.phoneNo.length > 10) {
+      errors.phoneNo = "phoneNo cannot exceed more than 10 digite";
+    }
+    return errors;
+  };
+
+  return (
+    <div className="container">
+      {Object.keys(formErrors).length === 0 && isSubmit ? (
+        <div className="ui message success">Signed in successfully</div>
+      ) : (
+        <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+      )}
+
+      <form onSubmit={handleSubmit}>
+        <h1>Login Form</h1>
+        <div className="ui divider"></div>
+        <div className="ui form">
+          <div className="field">
+            <label>Username</label>
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={formValues.username}
+              onChange={handleChange}
+            />
+          </div>
+          <p>{formErrors.username}</p>
+          <div className="field">
+            <label>Email</label>
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              value={formValues.email}
+              onChange={handleChange}
+            />
+          </div>
+          <p>{formErrors.email}</p>
+          <div className="field">
+            <label>PhoneNo</label>
+            <input
+              type="number"
+              name="phoneNo"
+              placeholder="phoneNo"
+              value={formValues.phoneNo}
+              onChange={handleChange}
+            />
+          </div>
+          <p>{formErrors.phoneNo}</p>
+          <button className="fluid ui button blue">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
+
+// import "./styles.css";
+
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello CodeSandbox</h1>
+//       <h2>Start editing to see some magic happen!</h2>
+//     </div>
+//   );
+// }
